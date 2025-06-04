257d9a9a8eb60e1239d3d534e487a664f482dd68
Abhinawap <149473741+Abhinawap@users.noreply.github.com>
2025-06-04 09:48:33 +0700

    Update page.tsx

--- my-ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
+++ my-ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
@@ -9,73 +9,56 @@ import NoResults from "@/app/components/ui/no-results";
 import MobileFilters from "./components/mobile-filter";
 import ProductCard from "@/app/components/ui/product-card";
 
-
 export const revalidate = 0;
 
-interface CategoryPageProps{
-    params :{
-        categoryId: string;
-    },
-    searchParams:{
-        colorId : string;
-        sizeId : string;
-    }
+interface CategoryPageProps {
+  params: Promise<{
+    categoryId: string;
+  }>;
+  searchParams: Promise<{
+    colorId?: string;
+    sizeId?: string;
+  }>;
 }
 
-const CategoryPage : React.FC<CategoryPageProps> = async ({
-    params,
-    searchParams
-}) => {
-    const asyncparams = await params;
-    const asearchParams = await searchParams; 
+const CategoryPage = async ({ params, searchParams }: CategoryPageProps) => {
+  const { categoryId } = await params;
+  const { colorId, sizeId } = await searchParams;
 
-    const products = await getProducts({
-        categoryId: asyncparams.categoryId,
-        colorId: asearchParams.colorId,
-        sizeId: asearchParams.sizeId
-    });
+  const products = await getProducts({
+    categoryId,
+    colorId,
+    sizeId,
+  });
 
-    
-    const sizes = await getSizes();
-    const colors = await getColors();
-    const category = await getCategory(asyncparams.categoryId);
+  const sizes = await getSizes();
+  const colors = await getColors();
+  const category = await getCategory(categoryId);
 
-    return ( 
-        <div className="bg-white">
-            <Container>
-                <Billboard
-                    data={category.billboard}/>
-                <div className="px-4 sm:px-6 lg:px-8 pb-24">
-                    <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-                        <MobileFilters sizes={sizes} colors={colors}/>
-                        <div className="hidden lg:block">
-                            <Filter 
-                            valueKey="sizeId"
-                            name="Sizes"
-                            data={sizes}
-                            />
-                            <Filter 
-                            valueKey="colorId"
-                            name="Colors"
-                            data={colors}
-                            />
-                        </div>
-                        <div className="mt-6 lg:col-span-4 lg:mt-0">
-                            {products.length === 0 && <NoResults/> }
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-4">
-                                {products.map((item) => (
-                                    <ProductCard
-                                    key={item.id}
-                                    data={item}
-                                    />
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Container>
+  return (
+    <div className="bg-white">
+      <Container>
+        <Billboard data={category.billboard} />
+        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            <MobileFilters sizes={sizes} colors={colors} />
+            <div className="hidden lg:block">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
-     );
-}
- 
+      </Container>
+    </div>
+  );
+};
+
 export default CategoryPage;
